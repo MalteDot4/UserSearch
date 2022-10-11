eb912781ad61c14910088e12748ef65a9eac4798
MalteDot4 <malte_reppke@web.de>
2022-10-11 19:23:17 +0200

    initial commit

---
+++ src/hooks/useFetchUser.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { apiConfig } from "../api/configData";
+
+function useFetchUser(gitUser) {
+  const [data, setData] = useState({});
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (gitUser)
+      fetch(`${apiConfig["url"]}${gitUser}`)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("couldn´t fetch user");
+          }
+          setError(null);
+          return res.json();
+        })
+        .then((result) => setData(result))
+        .catch((error) => setError(error.message));
+  }, [gitUser]);
+
+  return { data, error };
+}
+
+export default useFetchUser;
